1cb2ff5e84264461a75228162c1efe16a2d495e5
Alex Blake-Goudemond <alex@psybergate.co.za>
2025-11-24 21:37:50 +0200

    Initial Spring Boot Setup

---
+++ api/chatterbox-api/src/main/java/za/co/psybergate/ChatterboxApplication.java
@@ -0,0 +1,13 @@
+package za.co.psybergate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatterboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatterboxApplication.class, args);
+	}
+
+}
