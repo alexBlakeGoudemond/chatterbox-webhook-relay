35bfaf5fdf59eb7289d0d60ce870f723a7f008fb
Alex Blake-Goudemond <alex@psybergate.co.za>
2025-11-24 21:38:28 +0200

    Format everything

--- api/chatterbox-api/src/main/java/za/co/psybergate/ChatterboxApplication.java
+++ api/chatterbox-api/src/main/java/za/co/psybergate/ChatterboxApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChatterboxApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChatterboxApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChatterboxApplication.class, args);
+    }
 
 }
