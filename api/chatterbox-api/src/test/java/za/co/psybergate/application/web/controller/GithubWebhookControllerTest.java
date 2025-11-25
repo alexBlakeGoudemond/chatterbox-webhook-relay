b4b46cdde1e9644e0424a181e7fa4e1e099fc19a
Alex Blake-Goudemond <alex@psybergate.co.za>
2025-11-25 22:19:05 +0200

    Task/verify incoming GitHub webhook (#3)
    * Add Gif to README for fun
    
    * Create EncryptionUtilities
    
    * Add IsIdentical to EncryptionUtilities
    
    * Add Encryption work to Github Controller
    
    * Add X-Hub-Signature to sites-enabled and sites-available
    
    * Add Surefire, Failsafe and JaCoCo to pom
    
    * Using JaCoCo - extend existing tests for more test coverage
    
    * Get JAR working again using spring boot maven plugin
    
    * Add Warning log to confirm pipeline working
    
    * Add timezone to chatterbox-nginx to better understand nginx logs

--- api/chatterbox-api/src/test/java/za/co/psybergate/application/web/controller/GithubWebhookControllerTest.java
+++ api/chatterbox-api/src/test/java/za/co/psybergate/application/web/controller/GithubWebhookControllerTest.java
@@ -5,28 +5,62 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
+import za.co.psybergate.application.core.utility.EncryptionUtilities;
+import za.co.psybergate.application.core.utility.EncryptionUtilitiesImpl;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-// TODO BlakeGoudemond 2025/11/24 | Continue. Docker container working, just need to get working with lt (https)
 @WebMvcTest(GithubWebhookController.class)
+@Import(EncryptionUtilitiesImpl.class)
 public class GithubWebhookControllerTest {
 
     @Value("${api.prefix}")
     private String apiPrefix;
 
+    @Value("${webhook.github.secret}")
+    private String webhookSecret;
+
+    @Value("${webhook.github.payload}")
+    private String webhookPayload;
+
     @Autowired
     private MockMvc mockMvc;
 
-    @DisplayName("api/webhook/github works")
+    @Autowired
+    private EncryptionUtilities encryptionUtilities;
+
+    @DisplayName("Missing signature fails")
     @Test
-    void whenPostToGithubWebhook_WithJson_ThenAccepted() throws Exception {
+    void whenPostToGithubWebhook_WithJsonAndNoSignature_ThenFailure() throws Exception {
         mockMvc.perform(post(apiPrefix + "/webhook/github")
                         .contentType(APPLICATION_JSON)
-                        .content("{\"ping\":\"hello\"}"))
+                        .content(webhookPayload))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @DisplayName("Invalid signature fails")
+    @Test
+    void whenPostToGithubWebhook_WithJsonAndInvalidSignature_ThenFailure() throws Exception {
+        mockMvc.perform(post(apiPrefix + "/webhook/github")
+                        .contentType(APPLICATION_JSON)
+                        .content(webhookPayload)
+                        .header("X-Hub-Signature-256", webhookSecret))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @DisplayName("Encrypted signature succeeds")
+    @Test
+    void whenPostToGithubWebhook_WithJson_ThenAccepted() throws Exception {
+        String encryptedSignature = encryptionUtilities.encryptUsingSHA256(webhookSecret, webhookPayload);
+
+        mockMvc.perform(post(apiPrefix + "/webhook/github")
+                        .contentType(APPLICATION_JSON)
+                        .content(webhookPayload)
+                        .header("X-Hub-Signature-256", encryptedSignature))
                 .andExpect(status().isAccepted());
     }
 
