7e9c0eb236a8a7f98891a3e6a3bd6bc50636f55f
Alex Blake-Goudemond <alexgoudemond@gmail.com>
2025-11-25 06:13:40 +0200

    Task/flesh out GitHub webhook controller (#2)
    * First pass of the GithubWebhookController
    
    * Confirm Controller works with WebMVC test
    
    * Add Dockerfile and docker-compose
    
    * General improvements towards https
    
    * Added a README to the Api to showcase progress made

---
+++ api/chatterbox-api/src/test/java/za/co/psybergate/application/web/controller/GithubWebhookControllerTest.java
@@ -0,0 +1,33 @@
+package za.co.psybergate.application.web.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+// TODO BlakeGoudemond 2025/11/24 | Continue. Docker container working, just need to get working with lt (https)
+@WebMvcTest(GithubWebhookController.class)
+public class GithubWebhookControllerTest {
+
+    @Value("${api.prefix}")
+    private String apiPrefix;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @DisplayName("api/webhook/github works")
+    @Test
+    void whenPostToGithubWebhook_WithJson_ThenAccepted() throws Exception {
+        mockMvc.perform(post(apiPrefix + "/webhook/github")
+                        .contentType(APPLICATION_JSON)
+                        .content("{\"ping\":\"hello\"}"))
+                .andExpect(status().isAccepted());
+    }
+
+}
