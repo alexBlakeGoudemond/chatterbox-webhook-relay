be06ab32a3e6fa52e4a9f42814e892c28f017586
Alex Blake-Goudemond <alex@psybergate.co.za>
2025-11-30 11:37:21 +0200

    Task/bring in actuator (#8)
    * Bring in simple actuator
    
    * leverage internal port for chatterbox-api inside docker network
    
    * Bring in simple Metrics, Controler Advice and rearrange things
    
    * Review tests so that they pass
    
    * Confirm working as per normal
    
    * Reduce scope of the ControllerIT
    
    * Refactor WebhookFilter
    
    * Placed WebhookMetrics and Exceptions in infrastructure package
    
    * Jot down how Metrics are saved (Per Runtime)
    
    * Update readme

---
+++ api/chatterbox-api/src/main/java/za/co/psybergate/chatterbox/infrastructure/handler/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package za.co.psybergate.chatterbox.infrastructure.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+// TODO BlakeGoudemond 2025/11/30 | This will be used for problems within scope of the Controller
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+//    @ExceptionHandler(UnauthorizedException.class)
+//    public ResponseEntity<String> unauthorized(UnauthorizedException ex) {
+//        return ResponseEntity
+//                .status(HttpStatus.UNAUTHORIZED)
+//                .body(ex.getMessage());
+//    }
+
+}
--- api/chatterbox-api/src/main/java/za/co/psybergate/chatterbox/infrastructure/logging/WebhookLogger.java
+++ api/chatterbox-api/src/main/java/za/co/psybergate/chatterbox/infrastructure/logging/WebhookLogger.java
@@ -2,10 +2,11 @@ package za.co.psybergate.chatterbox.infrastructure.logging;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import za.co.psybergate.chatterbox.infrastructure.filter.WebhookFilter;
 
 @Component
 @Slf4j
-public class SignatureValidationLogger {
+public class WebhookLogger {
 
     public void logMissingSignature() {
         log.warn("[Signature] Missing signature");
@@ -19,5 +20,12 @@ public class SignatureValidationLogger {
         log.info("[Signature] Valid signature");
     }
 
+    public void logReceivedWebhookEvent(String event, String delivery) {
+        log.info("[Webhook] Received event={} delivery={}", event, delivery);
+    }
+
+    public void logCompletion(long ms) {
+        log.debug("[Webhook] Completed in {}ms", ms);
+    }
 
 }
