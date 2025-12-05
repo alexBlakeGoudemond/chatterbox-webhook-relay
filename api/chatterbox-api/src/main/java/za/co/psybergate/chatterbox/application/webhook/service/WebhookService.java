54104e77e2e18a15db845c4502d33cec5f15d67b
Alex Blake-Goudemond <alex@psybergate.co.za>
2025-12-05 22:08:53 +0200

    Split WebhookService into WebhookValidator

--- api/chatterbox-api/src/main/java/za/co/psybergate/chatterbox/application/webhook/service/WebhookService.java
+++ api/chatterbox-api/src/main/java/za/co/psybergate/chatterbox/application/webhook/service/WebhookService.java
@@ -2,15 +2,10 @@ package za.co.psybergate.chatterbox.application.webhook.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import za.co.psybergate.chatterbox.infrastructure.exception.BadRequestException;
-import za.co.psybergate.chatterbox.infrastructure.exception.UnrecognizedRequestException;
 
 public interface WebhookService {
 
     void process(String eventType, JsonNode rawBody);
-
-    void assertAcceptedEvent(String eventType) throws UnrecognizedRequestException;
-
-    void assertAcceptedRepository(String repositoryName) throws UnrecognizedRequestException;
 
     String getRepositoryName(JsonNode rawBody) throws BadRequestException;
 
