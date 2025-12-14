6cd1041682a69fa530c0f25b506ca8e0ff8c3af9
Alex Blake-Goudemond <alex@psybergate.co.za>
2025-12-14 16:15:44 +0200

    Refine metrics test for webhook.payload.successes

--- api/chatterbox-api/src/test/java/za/co/psybergate/chatterbox/application/infrastructure/web/filter/WebhookFilterIT.java
+++ api/chatterbox-api/src/test/java/za/co/psybergate/chatterbox/application/infrastructure/web/filter/WebhookFilterIT.java
@@ -1,19 +1,25 @@
 package za.co.psybergate.chatterbox.application.infrastructure.web.filter;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Timer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import za.co.psybergate.chatterbox.application.webhook.orchestration.GithubWebhookService;
 import za.co.psybergate.chatterbox.domain.utility.JsonConverter;
 import za.co.psybergate.chatterbox.domain.utility.PayloadCryptor;
 import za.co.psybergate.chatterbox.infrastructure.actuator.WebhookRuntimeMetrics;
+import za.co.psybergate.chatterbox.infrastructure.exception.InternalServerException;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -24,6 +30,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class WebhookFilterIT {
 
+    // TODO BlakeGoudemond 2025/12/14 | extract these settings to WebhookConfig
     @Value("${api.prefix}")
     private String apiPrefix;
 
@@ -39,15 +46,23 @@ public class WebhookFilterIT {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockitoBean
+    @Qualifier("githubWebhookServiceImpl")
+    private GithubWebhookService githubWebhookService;
+
     @Autowired
     private PayloadCryptor payloadCryptor;
 
     @Autowired
     private JsonConverter jsonConverter;
 
-    // TODO BlakeGoudemond 2025/12/14 | debug why not working
+    // TODO BlakeGoudemond 2025/12/14 | consider profile fr live tests with @EnabledIfEnvironmentVariable(named = "RUN_REAL_WEBHOOKS", matches = "true")
+    // TODO BlakeGoudemond 2025/12/14 | extract to method like performWebhook - then put in helper class
     @Test
     void filterMetricsAreRecorded() throws Exception {
+        Mockito.doNothing()
+                .when(githubWebhookService).process(Mockito.anyString(), Mockito.any(JsonNode.class));
+
         MockHttpServletRequestBuilder httpRequest = getHttpRequestValid(webhookSecret, readGithubPayload());
         mockMvc.perform(httpRequest)
                 .andReturn(); // status does not matter
@@ -59,13 +74,6 @@ public class WebhookFilterIT {
 
         Assertions.assertNotNull(counter);
         Assertions.assertEquals(1.0, counter.count());
-
-        Timer timer = meterRegistry
-                .find("http.requests.filter.duration")
-                .timer();
-
-        Assertions.assertNotNull(timer);
-        Assertions.assertEquals(1, timer.count());
     }
 
     private MockHttpServletRequestBuilder getHttpRequestValid(String payloadSecret, String payload) {
