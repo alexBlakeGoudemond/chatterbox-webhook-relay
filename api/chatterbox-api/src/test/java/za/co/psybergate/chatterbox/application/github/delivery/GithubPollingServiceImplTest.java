50be4fb5b8242a3abcdc8ad48cfe97ce83caeb10
Alex Blake-Goudemond <alex@psybergate.co.za>
2025-12-19 23:56:00 +0200

    Refactor current tests to reuse common logic

--- api/chatterbox-api/src/test/java/za/co/psybergate/chatterbox/application/github/delivery/GithubPollingServiceImplTest.java
+++ api/chatterbox-api/src/test/java/za/co/psybergate/chatterbox/application/github/delivery/GithubPollingServiceImplTest.java
@@ -1,13 +1,12 @@
 package za.co.psybergate.chatterbox.application.github.delivery;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.kohsuke.github.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import za.co.psybergate.chatterbox.application.exception.ApplicationException;
 import za.co.psybergate.chatterbox.infrastructure.config.ApplicationConfig;
 import za.co.psybergate.chatterbox.infrastructure.config.properties.ChatterboxSecurityApiGithubProperties;
 import za.co.psybergate.chatterbox.infrastructure.web.filter.WebhookFilter;
@@ -35,14 +34,24 @@ public class GithubPollingServiceImplTest {
     @Autowired
     private GithubPollingService githubPollingService;
 
+    private GitHub githubApi;
+
+    @BeforeEach
+    public void setup() {
+        try {
+            this.githubApi = new GitHubBuilder()
+                    .withOAuthToken(apiGithubProperties.getToken())
+                    .build();
+        } catch (IOException e) {
+            Assertions.fail("Unable to create the base Github POJO", e);
+        }
+    }
+
     @Tag("live-integration")
     @DisplayName("Can Query Commits")
     @Test
-    public void givenRepositoryDetails_AndStartingDate_WhenQueryCommits_ThenSuccess() throws IOException {
-        GitHub gitHub = new GitHubBuilder()
-                .withOAuthToken(apiGithubProperties.getToken())
-                .build();
-        GHRepository repository = gitHub.getRepository("psyAlexBlakeGoudemond/chatterbox");
+    public void givenRepositoryDetails_AndStartingDate_WhenQueryCommits_ThenSuccess() {
+        GHRepository repository = getGithubRepository();
         LocalDateTime lastReceivedUpdate = LocalDateTime.of(2025, 12, 15, 10, 0);
 
         List<GHCommit> commitsSince = githubPollingService.getCommitsSince(repository, lastReceivedUpdate, LocalDateTime.now());
@@ -54,15 +63,20 @@ public class GithubPollingServiceImplTest {
     @DisplayName("Can Query Pull Requests")
     @Test
     public void givenRepositoryDetails_AndStartingDate_WhenQueryPullRequests_ThenSuccess() throws IOException {
-        GitHub gitHub = new GitHubBuilder()
-                .withOAuthToken(apiGithubProperties.getToken())
-                .build();
-        GHRepository repository = gitHub.getRepository("psyAlexBlakeGoudemond/chatterbox");
+        GHRepository repository = getGithubRepository();
         LocalDateTime lastReceivedUpdate = LocalDateTime.of(2025, 12, 15, 10, 0);
 
         List<GHPullRequest> pullRequestsSince = githubPollingService.getPullRequestsSince(repository, lastReceivedUpdate, LocalDateTime.now());
         assertNotNull(pullRequestsSince);
         assertFalse(pullRequestsSince.isEmpty());
+    }
+
+    private GHRepository getGithubRepository() {
+        try {
+            return githubApi.getRepository("psyAlexBlakeGoudemond/chatterbox");
+        } catch (IOException e) {
+            throw new ApplicationException("Unable to retrieve repository", e);
+        }
     }
 
 }
