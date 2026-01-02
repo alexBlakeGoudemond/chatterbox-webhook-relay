9bbaebcef41306f3fecaa20155b6d4595a1ff5cb
Alex Blake-Goudemond <alex@psybergate.co.za>
2026-01-02 08:32:52 +0200

    tried to tidy up the Hikari log issues

--- api/chatterbox-api/src/test/java/za/co/psybergate/chatterbox/infrastructure/persistence/webhook/WebhookEventStoreJpaAdapterIT.java
+++ api/chatterbox-api/src/test/java/za/co/psybergate/chatterbox/infrastructure/persistence/webhook/WebhookEventStoreJpaAdapterIT.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import za.co.psybergate.chatterbox.application.webhook.processing.GithubEventExtractor;
@@ -16,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment =  SpringBootTest.WebEnvironment.DEFINED_PORT)
 @Transactional
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class WebhookEventStoreJpaAdapterIT {
 
     @Autowired
