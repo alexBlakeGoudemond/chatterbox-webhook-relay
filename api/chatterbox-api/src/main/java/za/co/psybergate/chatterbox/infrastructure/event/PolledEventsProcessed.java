17d82857c7c1e9ed874e46cfe443b96bf178428f
Alex Blake-Goudemond <alex@psybergate.co.za>
2026-01-09 18:01:40 +0200

    Bring in simple timestamps for Event Listeners (#36)

--- api/chatterbox-api/src/main/java/za/co/psybergate/chatterbox/infrastructure/event/PolledEventsProcessed.java
+++ api/chatterbox-api/src/main/java/za/co/psybergate/chatterbox/infrastructure/event/PolledEventsProcessed.java
@@ -1,5 +1,16 @@
 package za.co.psybergate.chatterbox.infrastructure.event;
 
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
 public class PolledEventsProcessed {
+
+    private final LocalDateTime eventDateTime;
+
+    public PolledEventsProcessed() {
+        this.eventDateTime = LocalDateTime.now();
+    }
 
 }
