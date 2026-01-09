5e254a28e1185d3e8ffa1cc0b9dea80522740af0
Alex Blake-Goudemond <alexgoudemond@gmail.com>
2026-01-09 18:01:40 +0200

    Bring in simple timestamps for Event Listeners (#36)

--- api/chatterbox-api/src/main/java/za/co/psybergate/chatterbox/infrastructure/event/WebhookEventProcessed.java
+++ api/chatterbox-api/src/main/java/za/co/psybergate/chatterbox/infrastructure/event/WebhookEventProcessed.java
@@ -1,5 +1,16 @@
 package za.co.psybergate.chatterbox.infrastructure.event;
 
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
 public class WebhookEventProcessed {
+
+    private final LocalDateTime eventDateTime;
+
+    public WebhookEventProcessed() {
+        eventDateTime = LocalDateTime.now();
+    }
 
 }
