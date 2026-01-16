8497d149b9ba7d84d1ee3a8dea806bb38e398214
Alex Blake-Goudemond <alexgoudemond@gmail.com>
2026-01-16 09:38:45 +0200

    Extract EmbeddedObjectDefinition to domain and name it DiscordEmbeddedObjectDefinition

--- api/chatterbox-api/src/main/java/za/co/psybergate/chatterbox/application/discord/factory/DiscordEmbeddedObjectFactory.java
+++ api/chatterbox-api/src/main/java/za/co/psybergate/chatterbox/application/discord/factory/DiscordEmbeddedObjectFactory.java
@@ -2,21 +2,21 @@ package za.co.psybergate.chatterbox.application.discord.factory;
 
 import org.apache.hc.core5.http.ClassicHttpResponse;
 import za.co.psybergate.chatterbox.application.exception.ApplicationException;
+import za.co.psybergate.chatterbox.domain.discord.DiscordEmbeddedObjectDefinition;
 import za.co.psybergate.chatterbox.domain.dto.GithubEventDto;
 import za.co.psybergate.chatterbox.domain.dto.HttpResponseDto;
-import za.co.psybergate.chatterbox.infrastructure.config.properties.ChatterboxDeliveryDiscordProperties;
 
 import java.util.Map;
 
 public interface DiscordEmbeddedObjectFactory {
 
     /// From a given [Map] of property values, create and populate the
-    /// [ChatterboxDeliveryDiscordProperties.EmbeddedObjectDefinition]
-    ChatterboxDeliveryDiscordProperties.EmbeddedObjectDefinition buildEmbeddedObjectDefinition(Map<String, String> values);
+    /// [DiscordEmbeddedObjectDefinition]
+    DiscordEmbeddedObjectDefinition buildEmbeddedObjectDefinition(Map<String, String> values);
 
     /// From a given [GithubEventDto] create a [Map] and leverage [DiscordEmbeddedObjectFactory#buildEmbeddedObjectDefinition(Map))]
-    /// to create a [ChatterboxDeliveryDiscordProperties.EmbeddedObjectDefinition]
-    ChatterboxDeliveryDiscordProperties.EmbeddedObjectDefinition buildEmbeddedObjectDefinition(GithubEventDto dto);
+    /// to create a [DiscordEmbeddedObjectDefinition]
+    DiscordEmbeddedObjectDefinition buildEmbeddedObjectDefinition(GithubEventDto dto);
 
     String getAsDiscordPayloadString(GithubEventDto eventDto) throws ApplicationException;
 
