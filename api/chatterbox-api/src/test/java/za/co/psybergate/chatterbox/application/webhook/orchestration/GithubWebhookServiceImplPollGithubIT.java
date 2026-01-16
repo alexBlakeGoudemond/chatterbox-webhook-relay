5193812ab646868e9b821ffc6e5f10d3d4103d37
Alex Blake-Goudemond <alexgoudemond@gmail.com>
2026-01-16 10:21:34 +0200

    Place GithubWebhookServiceImpl in infrastructure

--- api/chatterbox-api/src/test/java/za/co/psybergate/chatterbox/application/webhook/orchestration/GithubWebhookServiceImplPollGithubIT.java
+++ api/chatterbox-api/src/test/java/za/co/psybergate/chatterbox/application/webhook/orchestration/GithubWebhookServiceImplPollGithubIT.java
@@ -13,6 +13,7 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import za.co.psybergate.chatterbox.application.github.delivery.GithubPollingServiceImpl;
 import za.co.psybergate.chatterbox.application.webhook.ingest.WebhookRequestValidatorImpl;
+import za.co.psybergate.chatterbox.infrastructure.webhook.orchestration.GithubWebhookServiceImpl;
 import za.co.psybergate.chatterbox.infrastructure.webhook.processing.GithubEventExtractorImpl;
 import za.co.psybergate.chatterbox.application.webhook.routing.WebhookConfigurationResolverImpl;
 import za.co.psybergate.chatterbox.domain.dto.RepositoryDetail;
