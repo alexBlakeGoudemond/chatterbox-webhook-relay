e2a767040e74e1f993bbdd5b8d6f6c889b608ae3
Alex Blake-Goudemond <alex@psybergate.co.za>
2026-01-21 23:51:04 +0200

    Bring in za.co.psybergate.architecture.rules to assert Hexagonal rules are being followed

---
+++ api/chatterbox-api/src/test/java/za/co/psybergate/chatterbox/ArchitectureRulesTest.java
@@ -0,0 +1,12 @@
+package za.co.psybergate.chatterbox;
+
+import za.co.psybergate.architecture_rules.hexagon.HexagonalArchitectureAbstractTest;
+
+public class ArchitectureRulesTest extends HexagonalArchitectureAbstractTest {
+
+    @Override
+    protected String basePackage() {
+        return "za.co.psybergate.chatterbox";
+    }
+
+}
