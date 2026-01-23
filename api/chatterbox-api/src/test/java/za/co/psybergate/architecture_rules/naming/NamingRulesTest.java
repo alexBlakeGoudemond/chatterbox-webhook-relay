fdcab9efee496de69c73cf8c44f28ece4e670e87
Alex Blake-Goudemond <alex@psybergate.co.za>
2026-01-23 23:06:16 +0200

    Add NamingRulesTest

---
+++ api/chatterbox-api/src/test/java/za/co/psybergate/architecture_rules/naming/NamingRulesTest.java
@@ -0,0 +1,10 @@
+package za.co.psybergate.architecture_rules.naming;
+
+public class NamingRulesTest extends NamingRulesAbstractTest {
+
+    @Override
+    protected String basePackage() {
+        return "za.co.psybergate.chatterbox";
+    }
+
+}
