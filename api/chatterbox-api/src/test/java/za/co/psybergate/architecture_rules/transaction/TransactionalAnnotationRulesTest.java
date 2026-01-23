0c5ee2db2891030b3cb137a8d4c259db58e57acc
Alex Blake-Goudemond <alex@psybergate.co.za>
2026-01-23 22:41:37 +0200

    Added test TransactionalAnnotationRulesTest

---
+++ api/chatterbox-api/src/test/java/za/co/psybergate/architecture_rules/transaction/TransactionalAnnotationRulesTest.java
@@ -0,0 +1,10 @@
+package za.co.psybergate.architecture_rules.transaction;
+
+public class TransactionalAnnotationRulesTest extends TransactionalAnnotationAbstractTest {
+
+    @Override
+    protected String basePackage() {
+        return "za.co.psybergate.chatterbox";
+    }
+
+}
