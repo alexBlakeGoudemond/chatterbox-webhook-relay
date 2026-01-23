8a294e65871f171b10080fe6e36b739e7c136dac
Alex Blake-Goudemond <alex@psybergate.co.za>
2026-01-23 22:43:28 +0200

    Added test InjectionRulesTest

---
+++ api/chatterbox-api/src/test/java/za/co/psybergate/architecture_rules/injection/InjectionRulesTest.java
@@ -0,0 +1,10 @@
+package za.co.psybergate.architecture_rules.injection;
+
+public class InjectionRulesTest extends InjectionAbstractTest {
+
+    @Override
+    protected String basePackage() {
+        return "za.co.psybergate.chatterbox";
+    }
+
+}
