274ce2df280b55e7e800b7f5ceddd313c1782bd4
Alex Blake-Goudemond <alex@psybergate.co.za>
2026-01-23 23:11:15 +0200

    Rename NamingRulesTest to PlacementRulesTest

---
+++ api/chatterbox-api/src/test/java/za/co/psybergate/architecture_rules/structure/PlacementRulesTest.java
@@ -0,0 +1,10 @@
+package za.co.psybergate.architecture_rules.structure;
+
+public class PlacementRulesTest extends PlacementRulesAbstractTest {
+
+    @Override
+    protected String basePackage() {
+        return "za.co.psybergate.chatterbox";
+    }
+
+}
