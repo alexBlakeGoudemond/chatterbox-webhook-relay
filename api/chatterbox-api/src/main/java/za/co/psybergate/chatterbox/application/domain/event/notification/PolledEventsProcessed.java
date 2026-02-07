507f4833a6d606b3e8f8f1c52975c0befa0b77d5
Alex Blake-Goudemond <alex@psybergate.co.za>
2026-02-07 06:47:09 +0200

    Delete old TODO

--- api/chatterbox-api/src/main/java/za/co/psybergate/chatterbox/application/domain/event/notification/PolledEventsProcessed.java
+++ api/chatterbox-api/src/main/java/za/co/psybergate/chatterbox/application/domain/event/notification/PolledEventsProcessed.java
@@ -5,7 +5,6 @@ import lombok.ToString;
 
 import java.time.LocalDateTime;
 
-// TODO BlakeGoudemond 2026/01/30 | should we have a github impl of this - could poll Bitbucket?
 @Getter
 @ToString
 public class PolledEventsProcessed {
