8a24cb9b74509a5dddc618c9f9ba00ce9746db0f
Alex Blake-Goudemond <alexgoudemond@gmail.com>
2026-04-21 09:28:56 +0200

    Update Simple Application Test to use TestContainers; in the event that no docker container is running yet - just bootstrapping test after all

--- api/chatterbox-api/src/test/java/com/webhook/relay/chatterbox/ChatterboxApplicationTests.java
+++ api/chatterbox-api/src/test/java/com/webhook/relay/chatterbox/ChatterboxApplicationTests.java
@@ -1,11 +1,14 @@
 package com.webhook.relay.chatterbox;
 
+import com.webhook.relay.chatterbox.test.container.AbstractPostgresTestContainer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-class ChatterboxApplicationTests {
+@Testcontainers
+class ChatterboxApplicationTests extends AbstractPostgresTestContainer {
 
     @Test
     void contextLoads() {
